b75dcce5da799a443e3fa6169aa056cd292ba3e7
aosousa <aosousa@indracompany.com>
2021-07-01 17:06:39 -0300

    alter b2

--- src/main/java/com/andre_empresa/cursomc/domain/Categoria.java
+++ src/main/java/com/andre_empresa/cursomc/domain/Categoria.java
@@ -1,5 +1,5 @@
-package com.andre_empresa.cursomc.domain; branch teste1
-nova modificação
+package com.andre_empresa.cursomc.domain;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
